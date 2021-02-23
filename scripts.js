91c22d0d118963208637aeec6cb87fedc0224295
kioksianto <brenda.sianto@gmail.com>
2021-02-23 14:58:15 +0300

    dancing image

--- scripts.js
+++ scripts.js
@@ -2,42 +2,40 @@
 //Gender function
 
 function getGender(){
-var genders = document.getElementsByName("gender");
-if(genders[0].checked == true){
-    var gender = "male";
-}
-else if(genders[1].checked == true){
-    var gender = "female";
-}
-else{
-    return false;
-}    let gender = gender("gender")
-}
-//Birthday
-
-let dateOfBirth = document.getElementsByName("birthday")();
-let dayOfWeek = dateOfBirth.getDay();
-
-//Array list
-
-function getWeekName(weekNumber){
-    var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday','Friday','Saturday'];
-    return dayOfWeek [weekNumber];
-}
-//Getting the Akan name
-
-function decideAkanName (male, female){
-    let akanNameDataBase = {
-        'male': { 'Sunday':Kwasi,'Monday':Kwadwo,'Tuesday':Kwabeka,'Wednesday':Kwaku,'Thursday':Yaw,'Friday':Kofi,'Saturday':Kwame},
-        'female': { 'Sunday':Akosua,'Monday':Adwoa,'Tuesday':Abenaa,'Wednesday':Akua,'Thursday':Yaa,'Friday':Afua,'Saturday':Ama},
-    };
-    let akanName = akanNameDataBase [male][female];
-    return akanName;
-
-}
-//Final Message
-
-function myText() {
-    var finalMessage = 'Your Akan name is'+ ' ' + akanName;
-    alert (finalMessage);
-  }
+    var genders = document.getElementsByName("gender");
+    if(genders[0].checked == true){
+        var gender = "male";
+    }
+    else if(genders[1].checked == true){
+        var gender = "female";
+    }
+  
+    //Birthday
+    
+    let dateOfBirth = document.getElementsByName("birthday")();
+    let weekNumber = dateOfBirth.getDay();
+    
+    //Array list
+    
+    function getWeekName(weekNumber){
+        var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday','Friday','Saturday'];
+        return dayOfWeek [weekNumber];
+        
+    }
+    //Getting the Akan name
+    
+    function decideAkanName (male, female){
+        let akanNameDataBase = {
+            'male': { 'Sunday':Kwasi,'Monday':Kwadwo,'Tuesday':Kwabeka,'Wednesday':Kwaku,'Thursday':Yaw,'Friday':Kofi,'Saturday':Kwame},
+            'female': { 'Sunday':Akosua,'Monday':Adwoa,'Tuesday':Abenaa,'Wednesday':Akua,'Thursday':Yaa,'Friday':Afua,'Saturday':Ama},
+        };
+        let akanName = akanNameDataBase [male][female];
+        return akanName;
+    
+    }
+    //Final Message
+    
+    function myText() {
+        var finalMessage = 'Your Akan name is'+ ' ' + akanName;
+        alert (finalMessage);
+      }
